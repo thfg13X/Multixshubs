ea992c5b6154a30df26ee46a38747c737980cf54
thfg13X <etherealicuniversal@gmail.com>
2026-02-24 18:57:01 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,23 @@
-script_key = "trial"
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
 
-pcall(function()
-    loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/2872ef09b36cafac79a1420bacc1b75587ed50b426e3b135b3710a872bdd83d2.lua"))()
-end)
+local placeId = game.PlaceId
 
-script_key = "trial"
-
-pcall(function()
+-- memesea
+if placeId == 10260193230 then
+    
+    script_key = "trial"
     loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/f2aa9049c8fd2327d721dc75bb7655dc217de6ea5dc7760ca94597b458a3df2b.lua"))()
-end)
+
+-- tsb
+elseif placeId == 10449761463 
+    or placeId == 131048399685555 
+    or placeId == 12360882630 then
+    
+    script_key = "trial"
+    loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/2872ef09b36cafac79a1420bacc1b75587ed50b426e3b135b3710a872bdd83d2.lua"))()
+
+else
+    warn("Unsupported game. PlaceId:", placeId)
+end
